7a0904377781580ea54f5103cc7f846b53c8efa3
StanMalinovskii <malina_mami@mail.ru>
2023-10-25 13:47:57 +0300

    final version 1.1

--- clothing_store/script.js
+++ clothing_store/script.js
@@ -3,7 +3,7 @@ const burger = document.querySelector('.user-list__item-menu'); // find burger b
 // const menuClose = document.querySelector('.menu-close'); // find button x
 
 function toggleMenu() {
-	menuActive.classList.toggle('hidden'); //function remove or set class with name hidden in div with class .menu-active
+	menuActive.classList.toggle('hide'); //function remove or set class with name hidden in div with class .menu-active
 }
 
 burger.addEventListener('click', toggleMenu); // function will work upon click on burger
