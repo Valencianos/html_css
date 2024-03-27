0963d959d52519100010f86a3fb2641363f88404
StanMalinovskii <malina_mami@mail.ru>
2024-03-27 10:39:28 +0300

    10th hw added

---
+++ JavaScript/homework10/data.js
@@ -0,0 +1,34 @@
+const data = `
+[
+  {
+    "type": "general",
+    "setup": "Why do bees hum?",
+    "punchline": "Because they don't know the words.",
+    "id": 349
+  },
+  {
+    "type": "general",
+    "setup": "What does C.S. Lewis keep at the back of his wardrobe?",
+    "punchline": "Narnia business!",
+    "id": 22
+  },
+  {
+    "type": "general",
+    "setup": "What are the strongest days of the week?",
+    "punchline": "Saturday and Sunday...the rest are weekdays.",
+    "id": 156
+  },
+  {
+    "type": "general",
+    "setup": "What kind of pants do ghosts wear?",
+    "punchline": "Boo jeans.",
+    "id": 261
+  },
+  {
+    "type": "general",
+    "setup": "What’s the difference between an African elephant and an Indian elephant?",
+    "punchline": "About 5000 miles.",
+    "id": 284
+  }
+]
+`
