40820bf61e5bd38f38e78a8209057833d3ef74f6
StanMalinovskii <malina_mami@mail.ru>
2024-03-29 17:24:17 +0300

    draft to 11 sem added

---
+++ JavaScript/seminar11/data.js
@@ -0,0 +1,21 @@
+const dataProduct = `
+[
+  {
+    "id": 1,
+    "img": "1",
+    "title": "MANGO  PEOPLE  T-SHIRT",
+    "price": "$300",
+    "color": "red",
+    "size": "Xl",
+    "quantity": 2
+  },
+  {
+    "id": 2,
+    "img": "2",
+    "title": "ELLERY X M'O CAPSULE",
+    "price": "$52",
+    "color": "black",
+    "size": "Xl",
+    "quantity": 1
+  }
+]`
