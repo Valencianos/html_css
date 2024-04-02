295a419f9ebe29c068dab881dadd025297138196
StanMalinovskii <malina_mami@mail.ru>
2024-04-02 12:05:13 +0300

    11hw added

--- clothing_store/script.js
+++ clothing_store/script.js
@@ -8,3 +8,38 @@ function toggleMenu() {
 
 burger.addEventListener('click', toggleMenu); // function will work upon click on burger
 // menuClose.addEventListener('click', toggleMenu); // function will work upon click on x
+
+const url = './data.json';
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`mistake ${error}`);
+  }
+}
+
+const data = await getData(url);
+
+const cards = document.querySelector('.cards');
+data.forEach(element => {
+  cards.insertAdjacentHTML('beforeend', 
+  `
+  <div class="card">
+    <div class="card__img" style="background-image: url(${element.img})">
+      <div class="card__dark">
+        <button class="card__button">
+          <img src="./img/Forma 1 (3).svg" alt="add to basket button">
+          <p class="card__button_text">Add to Cart</p>
+        </button>
+      </div>
+    </div>
+    <div class="card__text">
+      <h5 class="card__text_title">${element.title}</h6>
+      <p class="card__text_desc">${element.desc}</p>
+      <p class="card__text_price">$${element.price}</p>
+    </div>
+  </div>
+  `)
+});
