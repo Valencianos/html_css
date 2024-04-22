e6184629352f36eee9570a67d07582367ee32b6e
StanMalinovskii <malina_mami@mail.ru>
2024-04-22 09:25:20 +0300

    merge

--- JavaScript/ECMAScript/seminar3/script.js
+++ JavaScript/ECMAScript/seminar3/script.js
@@ -77,7 +77,7 @@ class Animal {
 class Dog extends Animal {
   breed;
   constructor (name, breed) {
-    super(name, breed);
+    super(name);
     this.breed = breed;
   }
   fetch () {
@@ -94,3 +94,41 @@ const dog1 = new Dog("Бобик", "Дворняжка");
 dog1.speak(); // Вывод: Животное Бобик издает звук.
 console.log(dog1.breed); // Вывод: Дворняжка
 dog1.fetch(); // Вывод: Собака Бобик принесла мяч.
+
+// Создайте класс Product, который будет представлять товар в магазине. У товара должны быть следующие свойства:
+// name (строка) - имя товара.
+// price (число) - цена товара за одну единицу.
+
+class Product {
+  constructor (name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+// Создайте класс ShoppingCart, представляющий корзину интернет-магазина. Конструктор класса ShoppingCart должен принимать два параметра:
+// customerName (строка) - имя покупателя.
+// initialTotalCost (число) - начальная общая стоимость заказа (может быть равна 0, если корзина пуста).
+// У класса ShoppingCart должен быть метод addItem(product, quantity), который позволяет добавить товар в корзину. Метод должен принимать два параметра:
+// product (объект класса Product) - товар, который добавляется в корзину.
+// quantity (число, по умолчанию 1) - количество единиц товара, которое нужно добавить в корзину. Если quantity не указано, считается, что добавляется одна единица товара.
+// У класса ShoppingCart должен быть метод getCurrentTotalCost(), который возвращает текущую общую стоимость заказа.
+// У класса ShoppingCart должен быть метод checkout(), который оформляет заказ и выводит сообщение с общей стоимостью заказа и благодарностью за покупку. Формат вывода сообщения: "Заказ оформлен для {имя покупателя}. Общая стоимость заказа: {общая стоимость} рублей. Спасибо за покупку!".
+
+class ShoppingCart {
+  constructor (customerName, initialTotalCost=0) {
+    this.customerName = customerName;
+    this.initialTotalCost = initialTotalCost;
+    this.items = [];
+  }
+  addItem(product, quantity=1) {
+    this.items.push({product, quantity});
+    this.initialTotalCost += product.price * quantity;
+  }
+  getCurrentTotalCost() {
+    return this.initialTotalCost;
+  }
+  checkout() {
+    console.log(`Заказ оформлен для ${this.customerName}. Общая стоимость заказа: ${this.initialTotalCost} рублей. Спасибо за покупку!`);
+  }
+}
