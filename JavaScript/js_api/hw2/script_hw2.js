848944dacc59d6fa92fbb971d323c6e067bd2f02
StanMalinovskii <malina_mami@mail.ru>
2024-06-14 14:33:51 +0300

    2nd hw added

--- JavaScript/js_api/hw2/script_hw2.js
+++ JavaScript/js_api/hw2/script_hw2.js
@@ -11,3 +11,47 @@
 // 4. Слайдер должен циклически переключаться между изображениями, то есть после последнего изображения должно отображаться первое, и наоборот.
 // 5. Добавьте стилизацию для слайдера и элементов интерфейса с использованием CSS для улучшения внешнего вида.
 
+
+const sliderImages = document.querySelectorAll('.slider__image');
+const navigationDots = document.querySelectorAll('.navigation__link');
+const previousButton = document.querySelector('.previous__btn');
+const nextButton = document.querySelector('.next__btn');
+
+let currentIndex = 0;
+
+function showImage(index) {
+  sliderImages.forEach(image => image.classList.remove('active'));
+  navigationDots.forEach(dot => dot.classList.remove('active'));
+  sliderImages[index].classList.add('active');
+  navigationDots[index].classList.add('active');
+}
+
+function previousImage() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = sliderImages.length - 1;
+  }
+  showImage(currentIndex);
+}
+
+function nextImage() {
+  currentIndex++;
+  if (currentIndex >= sliderImages.length) {
+    currentIndex = 0;
+  }
+  showImage(currentIndex);
+}
+
+function switchToImage(index) {
+  currentIndex = index;
+  showImage(currentIndex);
+}
+
+previousButton.addEventListener('click', previousImage);
+nextButton.addEventListener('click', nextImage);
+
+navigationDots.forEach((dot, index) => {
+  dot.addEventListener('click', () => switchToImage(index));
+});
+
+showImage(currentIndex);
