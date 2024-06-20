c5ba00c66e38266963322a90c8c1aa9f6868c64b
StanMalinovskii <malina_mami@mail.ru>
2024-06-20 17:37:26 +0300

    cli updated

--- project_interno/src/App.vue
+++ project_interno/src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderComponent></HeaderComponent>
+  <MainHeroComponent></MainHeroComponent>
+  <MainComponent></MainComponent>
+  <FooterComponent></FooterComponent>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainHeroComponent from './components/MainHeroComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComponent,
+    MainHeroComponent,
+    MainComponent,
+    FooterComponent,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url(../src/assets/styles/style.css);
 </style>
--- project_interno/src/assets/scripts/news_and_articles.js
+++ project_interno/src/assets/scripts/news_and_articles.js
@@ -1,49 +1,4 @@
-const app = Vue.createApp({
-  data() {
-    return {
-      articles: [
-        {
-          title: 'Создадим лучший макет перепланировки',
-          category: 'Дизайн кухни',
-          date: '26 Декабрь,2022',
-          img: './img/kitchen-design.png'
-        },
-        {
-          title: 'Лучшие интерьерные идеи по низкой цене',
-          category: 'Дизайн для жизни',
-          date: '22 Декабрь,2022',
-          img: './img/lifestyle-design.png'
-        },
-        {
-          title: 'Лучшие интерьерные решения для офисов',
-          category: 'Дизайн Интерьера',
-          date: '25 Декабрь,2022',
-          img: './img/interior-design.png'
-        },
-        {
-          title: 'Создадим лучший макет перепланировки',
-          category: 'Дизайн кухни',
-          date: '26 Декабрь,2022',
-          img: './img/kitchen-design_2.png'
-        },
-        {
-          title: 'Лучшие интерьерные идеи по низкой цене',
-          category: 'Дизайн для жизни',
-          date: '22 Декабрь,2022',
-          img: './img/lifestyle-design_2.png'
-        },
-        {
-          title: 'Лучшие интерьерные решения для офисов',
-          category: 'Дизайн Интерьера',
-          date: '25 Декабрь,2022',
-          img: './img/interior-design_2.png'
-        }
-      ]
-    }
-  }
-})
 
-app.mount('#article__box')
 
 
 
