d7178b4f29870d80416aeca03ef61ee788b45eda
StanMalinovskii <malina_mami@mail.ru>
2024-07-02 13:56:14 +0300

    new update

---
+++ project_interno/src/components/MainComponent.vue
@@ -0,0 +1,25 @@
+<template lang="">
+  <main class="main">
+    <AboutComponent></AboutComponent>
+    <PromoComponent></PromoComponent>
+    <ArticleComponent></ArticleComponent>
+
+  </main>
+</template>
+<script>
+import AboutComponent from './AboutComponent.vue'
+import PromoComponent from './PromoComponent.vue'
+import ArticleComponent from './ArticleComponent.vue'
+
+export default {
+  name: "MainComponent",
+  components: {
+    AboutComponent,
+    PromoComponent,
+    ArticleComponent
+  }
+}
+</script>
+<style lang="">
+  
+</style>
---
+++ project_interno/src/components/PromoComponent.vue
@@ -0,0 +1,44 @@
+<template lang="">
+  <section class="promo">
+    <div class="promo__wrapper">
+      <div class="promo__box" v-for="promo in promos" :key="promo.id">
+        <p class="promo__selection">{{ promo.qty }}</p>
+        <p class="promo__text">{{ promo.desc }}</p>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "PromoComponent",
+  data() {
+    return {
+      promos: [
+        {
+          id: 1,
+          qty: 12,
+          desc: 'Лет опыта'
+        },
+        {
+          id: 2,
+          qty: 85,
+          desc: 'Успешных проектов'
+        },
+        {
+          id: 3,
+          qty: 15,
+          desc: 'Проектов в работе'
+        },
+        {
+          id: 4,
+          qty: 95,
+          desc: 'Счастливых клиентов'
+        }
+      ]
+    }
+  },
+}
+</script>
+<style lang="">
+  
+</style>
