f568d752996abd7c3e28dafa9709f835c6247134
StanMalinovskii <malina_mami@mail.ru>
2024-07-02 13:24:57 +0300

    hw5 added

---
+++ JavaScript/Vue/homework5/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ProductDetailsComp></ProductDetailsComp>
+  </div>
+</template>
+
+<script>
+import ProductDetailsComp from './components/ProductDetailsComp'
+
+export default {
+  name: 'App',
+  components: {
+    ProductDetailsComp,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
